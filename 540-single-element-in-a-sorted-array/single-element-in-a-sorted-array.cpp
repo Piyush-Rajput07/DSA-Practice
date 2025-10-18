c8e51b62bf725851283b2667dee82de1f763d548
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-10-18 20:29:12 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
-        int n = nums.size();
-        int st = 0, end = n - 1;
+        int st = 0, end = nums.size() - 1;
 
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[end] != nums[end-1]) return nums[end];
+        
         while(st <= end) {
-            int mid = st + (end - st)/2;
-            //handling edge cases when unique ele is on st or end
-            if(mid == 0 && nums[0] != nums[1]) return nums[0];
-            if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[n-1];
-            
-            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1])    
+            int mid = st + (end-st)/2;
+            if((nums[mid-1] != nums[mid]) && (nums[mid] != nums[mid+1])) {
                 return nums[mid];
-
-            if(mid%2 == 0) { // even no. of elements on both sides
-                if(nums[mid-1] == nums[mid])
-                    end = mid - 1;
-                else 
-                    st = mid + 1;
             }
-            else { // odd no. of elements on both sides
-                if(nums[mid-1] == nums[mid])    
-                    st = mid + 1;
-                else 
+            else if(mid%2 == 0) {
+                if(nums[mid-1] == nums[mid]) { //ele is on left
                     end = mid - 1;
+                } else { //ele is on right
+                    st = mid + 1;
+                }
+            }
+            else {
+                if(nums[mid-1] == nums[mid]) { //ele is on right
+                    st = mid + 1;
+                } else { //ele is on left
+                    end = mid - 1;
+                }
             }
         }
         return -1;
