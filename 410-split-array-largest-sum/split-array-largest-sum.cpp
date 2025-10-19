d3f6c165f666a0c24fce210b751ab807b25bf7ac
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-10-19 14:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (11.41%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int k, int maxAllowedSum) {
+        int subArrays = 1, sum = 0;
+        for(int val : nums) {
+            if(sum + val <= maxAllowedSum) {
+                sum += val;
+            }
+            else {
+                subArrays++;
+                sum = val;
+            }
+        }
+        return (subArrays <= k) ? true : false;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int sum = 0, ans = 0;
+        for(int val : nums) {
+            sum += val;
+        }
+
+        int st = *max_element(nums.begin(), nums.end()), end = sum;
+
+        while(st <= end) {
+            int mid = st + (end-st)/2;
+
+            if(isPossible(nums, k, mid)) { //we'll try to find the min -> move left
+                ans = mid;
+                end = mid - 1;
+            }
+            else { //we'l find the min value on right
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
