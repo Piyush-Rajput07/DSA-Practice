49743a256472516d2bc0940736868be6e00d6d24
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-11-15 15:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.40%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int binSearch(vector<int>& arr, int tar, int st, int end) {
+        if(st > end) {
+            return -1;
+        }
+        int mid = st + (end - st)/2;
+
+        if(arr[mid] == tar) {
+            return mid;
+        }
+        else if(arr[mid] < tar) {
+            return binSearch(arr, tar, mid + 1, end);
+        }
+        else {
+            return binSearch(arr, tar, st, mid - 1);
+        }
+    }
+
+    int search(vector<int>& arr, int tar) {
+        int st = 0, end = arr.size() - 1;
+        
+        return binSearch(arr, tar, st, end);
+    }
+};
