1ff211b0178d4668cd385faad7762159c20e92f9
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-11-20 10:37:57 +0530

    Time: 2 ms (46.84%) | Memory: 14 MB (68.68%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -3,11 +3,11 @@ public:
     int findFinalValue(vector<int>& nums, int original) {
         int n = nums.size();
 
+        sort(nums.begin(), nums.end());
+
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(nums[j] == original) {
-                    original = 2 * original;
-                }
+            if(nums[i] == original) {
+                original = 2 * original;
             }
         }
         return original;
