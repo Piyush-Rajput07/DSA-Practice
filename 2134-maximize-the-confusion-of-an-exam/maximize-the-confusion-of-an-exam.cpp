80088da8bb57ef4c2f446543d74b09799cf28740
Piyush-Rajput07 <thakurpiyushsingh.22@gmail.com>
2025-12-03 11:34:19 +0530

    Time: 41 ms (5.49%) | Memory: 12.4 MB (80.07%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -4,36 +4,14 @@ public:
         int n = answerKey.length();
         int result = 0;
 
-        //Case-1 => F -> T
-        int CountF = 0;
+        unordered_map<char, int> mp;
         int i = 0, j = 0;
 
         while(j < n) {
-            if(answerKey[j] == 'F') {
-                CountF++;
-            }
+            mp[answerKey[j]]++;
 
-            while(CountF > k) {
-                if(answerKey[i] == 'F')
-                    CountF--;
-                i++;
-            }
-
-            result = max(result, j-i+1);
-            j++;
-        }
-
-        //Case-2 => T -> F
-        int CountT = 0;
-        i = 0, j = 0;
-
-        while(j < n) {
-            if(answerKey[j] == 'T') 
-                CountT++;
-
-            while(CountT > k) {
-                if(answerKey[i] == 'T')
-                    CountT--;
+            while(i < n && min(mp['T'], mp['F']) > k) {
+                mp[answerKey[i]]--;
                 i++;
             }
 
